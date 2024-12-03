28a413c979dbe879dbe421a768b6b6d4e6e03a02
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-12-03 23:24:07 +0700

    [VPM] Fix warn message

--- src/components/ElementPlus/ElementTag.tsx
+++ src/components/ElementPlus/ElementTag.tsx
@@ -1,11 +1,10 @@
 import { ElTag, type TagProps } from 'element-plus'
-import { ref, useSlots, type CSSProperties, defineExpose } from 'vue'
+import { useSlots, type CSSProperties } from 'vue'
 import type { JSX } from 'vue/jsx-runtime'
 import { generateElColor } from '@/utils/color'
 
 const ElementTag = (props: Partial<TagProps>): JSX.Element => {
   const slots = useSlots()
-  const tagRef = ref<InstanceType<typeof ElTag>>()
 
   let elStyles: CSSProperties = {}
   if (props.color) {
@@ -13,10 +12,6 @@ const ElementTag = (props: Partial<TagProps>): JSX.Element => {
     elStyles = colors
     elStyles.backgroundColor = `var(${colors['--el-color-primary-light-5']})`
   }
-
-  defineExpose({
-    tagRef
-  })
 
   return (
     <ElTag {...props} style={elStyles}>
